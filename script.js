654eef22f39ce86fef3b9b558bdcf4f580cc51fa
Karoline Correa <ckaroline226@gmail.com>
2022-11-07 22:14:02 -0300

    Estudando o projeto

---
+++ script.js
@@ -0,0 +1,15 @@
+var button = document.querySelector('button')
+const name = document.querySelector("gerar")
+
+function gerar() {
+    name.style.color = "blue";
+}
+
+function Frase() { 
+    "Olá Marilene"
+    "Não faça da sua vida um submundo do alcool e das drogas mas sim a glória dos seus sonhos"
+    "Heleno..."
+
+}
+
+Frase()
