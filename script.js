3f0f9eaded94b16095117e43603d96efc085573e
Karoline Correa <ckaroline226@gmail.com>
2022-11-16 22:20:56 -0300

    Alterando a funcionalidade do projeto

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-// function gerar() {
-//     let botao = document.getElementById("button")
+var Frases = [
+    "Olá Marilene",
+    "Não faça da sua vida um submundo do alcool e das drogas mas sim a glória dos seus sonhos",
+    "Heleno...",
+    "Como dizia Michael Jackson: AU"
+];
+
+var gerar = 
+
+    Frases[Math.floor(Math.random()*Frases.length)]
+
+
+//     // const frase = document.getElementById('li').value
+
+//     function gerar() {
+//     let botao = document.innerHTML("button")
     
 
-//     botao.onclick = function () {
-//         gerar() 
-//     }
+//     // botao.onclick = function () {
+//     //     gerar() 
+//     // }
 // }
 
 
 
-// button.addEventListener('click', button)
+// // button.addEventListener('click', button)
 
-function Frases() { 
-    'Olá Marilene'
-    'Não faça da sua vida um submundo do alcool e das drogas mas sim a glória dos seus sonhos'
-    'Heleno...'
-    'Como dizia Michael Jackson: AU'
-}
 
-Frases()
+    
+
 
 
