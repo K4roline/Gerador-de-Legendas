3a7fba9a31822ccf3d8dd62121616552b48ba4a9
Karoline Correa <ckaroline226@gmail.com>
2022-11-21 22:17:50 -0300

    Estudando a funcionaldade do botao

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ var Frases = [
     "Não faça da sua vida um submundo do alcool e das drogas mas sim a glória dos seus sonhos",
     "Heleno...",
     "Como dizia Michael Jackson: AU"
-];
+]
 
-var gerar = 
-
-    Frases[Math.floor(Math.random()*Frases.length)]
-
+var gerar =  Frases[Math.floor(Math.random()*Frases.length)]
+console.log(Frases[gerar])
 
     // const frase = document.getElementById('li').value
 
-    function gerar() {
-    let botao = document.innerHTML("button")
+    // function gerar() {
+    // let botao = document.innerHTML("button")
     
 
     // botao.onclick = function () {
     //     gerar() 
     // }
-}
+// }
 
 
 
