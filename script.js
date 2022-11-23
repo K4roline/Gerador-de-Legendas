85ae9fe72de89ceac940532d0a8acaf5eede80bc
Karoline Correa <ckaroline226@gmail.com>
2022-11-22 22:48:15 -0300

    Adicionando a funcionalidade de sorteio com o botao

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-var Frases = [
+function gerando() { 
+    let Frases = [
     "Olá Marilene",
     "Não faça da sua vida um submundo do alcool e das drogas mas sim a glória dos seus sonhos",
     "Heleno...",
-    "Como dizia Michael Jackson: AU"
-]
+    "Como dizia Michael Jackson: AU",
+    ]
 
-var gerar =  Frases[Math.floor(Math.random()*Frases.length)]
-console.log(Frases[gerar])
+    let gerar = Math.floor(Math.random()*Frases.length)
+    console.log(Frases[gerar])
+}
 
-    // const frase = document.getElementById('li').value
-
-    // function gerar() {
-    // let botao = document.innerHTML("button")
-    
-
-    // botao.onclick = function () {
-    //     gerar() 
-    // }
-// }
-
-
-
-// button.addEventListener('click', button)
+    var botao = document.getElementById("gerando")
+  
 
 
     
